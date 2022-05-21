14dd99a6994827cc06f867eb7569419b89d7c0aa
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2022-05-21 09:14:58 +0200

    Update ExploreBergen.js

--- components/pages/home/ExploreBergen.js
+++ components/pages/home/ExploreBergen.js
@@ -13,7 +13,7 @@ export const ExploreBergen = () => {
     <ExploreContainer>
       {size.width >= SCREEN.tablet ? (
         <>
-          <Heading size="2">We in Holiday have the best places to stay, handpicked for you!</Heading>
+          <Heading size="2">We in Holidaze have the best places to stay, handpicked for you!</Heading>
           <Paragraph>
             We work hard to find the best local places. We connect you directly with them, so you get a better holiday
             and you both get a fairer deal.
