765089651d45c7e143aeb7e159ddd818c8057ffa
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2022-05-26 10:53:36 +0200

    Update HotelOptions.js

--- components/pages/detailpage/HotelOptions.js
+++ components/pages/detailpage/HotelOptions.js
@@ -5,18 +5,19 @@ import Icon, { icons } from "../../../constants/icons";
 import { StyledMobileButton } from "../../../styles/buttons/StyledMobileButton.styled";
 import { StyledSelect } from "../../forms/styles/StyledSelect.styled";
 import { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export const HotelOptions = ({ options, id }) => {
   const [roomOption, setRoomOption] = useState("Choose room");
 
-  const handleInput = newValue => {
+  const handleInput = (newValue) => {
     setRoomOption(newValue.value);
     return newValue;
   };
 
   return (
     <div>
-      <Paragraph>{roomOption}</Paragraph>
+      <Form.Label>{roomOption}</Form.Label>
       <StyledSelect
         options={options}
         instanceId="select_one"
@@ -27,7 +28,7 @@ export const HotelOptions = ({ options, id }) => {
 
       <Link href={{ pathname: `/enquire/${id}`, query: { room: roomOption } }} className="mt-4">
         <StyledMobileButton className="btn primary-btn mt-4" role="button">
-          <Icon icon={icons.map(icon => icon.bag)} color="white" fontSize="16px" className="me-2" />
+          <Icon icon={icons.map((icon) => icon.bag)} color="white" fontSize="16px" className="me-2" />
           Enquire
         </StyledMobileButton>
       </Link>
