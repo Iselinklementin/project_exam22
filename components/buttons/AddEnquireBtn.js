0ea8e51178633c2a862ff2ed2638e17b60252937
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2022-05-26 10:08:49 +0200

    addEnq-btn

--- components/buttons/AddEnquireBtn.js
+++ components/buttons/AddEnquireBtn.js
@@ -5,7 +5,7 @@ import Add from "../../assets/add_stay.svg";
 
 export const AddEnquireBtn = () => {
   return (
-    <Link href="/add">
+    <Link href="/add" passHref>
       <StyledAddBtn className="btn btn-primary my-3">
         <Image src={Add} width="34px" height="36px" />
       </StyledAddBtn>
--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,600;0,700;1,400&family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
